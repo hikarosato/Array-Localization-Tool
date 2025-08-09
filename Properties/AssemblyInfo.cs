cdbc7f6d4f3da4d4cee54103a3d032ff2ab292e9
Hikaro <35894722+hikarosato@users.noreply.github.com>
2025-08-10 01:24:57 +0300

    2.5 fix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly
 )]
 
-[assembly: AssemblyVersion("2.4.2.0")]
-[assembly: AssemblyFileVersion("2.4.2.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
